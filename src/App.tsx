5ef2b8f0e8c83b5a291375bc1bb128c34cf1ee67
odling <decodeafterme@gmail.com>
2024-01-24 16:32:27 +0300

    [FEATURE] Add husky hooks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import "./App.css";
-import { MainLayout } from "./components";
+import './App.css';
+import { MainLayout } from './components';
 
 function App() {
   return (
     <>
       <MainLayout />
-      <div className="w-unit-7xl h-[200px] flex items-end">
-      </div>
     </>
   );
 }
