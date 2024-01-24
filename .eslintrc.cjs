faf779a0f077953ad81674e44e78446e5f39e1e3
odling <decodeafterme@gmail.com>
2024-01-24 16:28:19 +0300

    [INITIAL COMMIT]

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended"
+  ],
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { MainLayout } from "./components";
+
+function App() {
+  return (
+    <>
+      <MainLayout />
+      <div className="w-unit-7xl h-[200px] flex items-end">
+      </div>
+    </>
+  );
+}
+
+export default App;
