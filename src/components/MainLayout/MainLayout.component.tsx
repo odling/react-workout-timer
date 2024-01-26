91e36126aa231fb2c8d261707d209bf01edf51a9
odling <decodeafterme@gmail.com>
2024-01-26 09:52:02 +0300

    [FIX] fix 100vh issue.

--- src/components/MainLayout/MainLayout.component.tsx
+++ src/components/MainLayout/MainLayout.component.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
   return (
-    <div className="flex justify-center w-full h-[100vh] bg-background">
+    <div className="flex justify-center w-full h-full bg-red-500">
       <header></header>
       <main className="container h-full w-full">
         <Outlet />
