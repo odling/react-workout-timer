509fa5a47f7c77cdc485c796c898d5cb19912dbf
odling <decodeafterme@gmail.com>
2024-01-29 16:56:52 +0300

    [FIX] home page.

--- src/pages/Home/HomePage.component.tsx
+++ src/pages/Home/HomePage.component.tsx
@@ -4,8 +4,8 @@ import { useNavigate } from 'react-router';
 const HomePage = () => {
   const navigate = useNavigate();
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center gap-unit-xl from-primary to-success bg-gradient-to-br bg-clip-text text-xl text-center px-unit-md">
-      <h1 className="text-transparent font-semibold text-3xl mt-unit-xl">Welcome</h1>
+    <div className="w-full h-full flex flex-col justify-center items-center gap-unit-xl from-primary to-success bg-gradient-to-br bg-clip-text font-semibold text-xl text-center px-unit-md">
+      <h1 className="text-transparent font-bold text-3xl">Welcome</h1>
       <p className="text-transparent">Ready to transform your body?</p>
       <p className="text-transparent">
         This app is designed to help you achieve your goals. Just follow the workouts and remember,
@@ -13,7 +13,7 @@ const HomePage = () => {
       <p className="text-transparent">Hard work pays off!</p>
       <Button
         size="lg"
-        variant="light"
+        variant="faded"
         onPress={() => navigate('workouts')}
         className="bg-gradient-to-l bg-clip-text from-success to-primary"
       >
